68cd7439775e5cea31c9c3db03dcc27bcd4b83d3
Sagar ray <raysagar807@gmail.com>
2023-01-09 16:44:47 +0530

    first file

---
+++ A. Ultra-Fast Mathematician.cpp
@@ -0,0 +1,23 @@
+A. Ultra-Fast Mathematician
+
+#include iostream
+#include cstring
+using namespace std;
+ 
+int main()
+{
+    string s1,s2;
+    cins1s2;
+    
+    string final_ans=;
+    for(int i=0;is1.length();i++)
+    {
+        if(s1[i]!=s2[i])
+        final_ans+='1';
+        else
+        final_ans+='0';
+    }
+ 
+    coutfinal_ansendl;
+    return 0;
+}
